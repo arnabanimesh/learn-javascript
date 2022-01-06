99d05205648fa4f8be8eb37ab616af3e741029d2
Arnab Animesh Das <arnabanimesh.a@gmail.com>
2022-01-06 10:25:10 +0530

    Insert semicolons in js files

--- leadtracker_chrome_ext/index.js
+++ leadtracker_chrome_ext/index.js
@@ -1,82 +1,82 @@
-const inputBtn = document.getElementById("input-btn")
-const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("tab-btn")
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-let myLeads = []
+let myLeads = [];
 
-if (localStorage.hasOwnProperty("myLeads")){
-    myLeads = JSON.parse(localStorage.getItem("myLeads"))
+if (localStorage.hasOwnProperty("myLeads")) {
+    myLeads = JSON.parse(localStorage.getItem("myLeads"));
     if (Object.prototype.toString.call(myLeads) != '[object Array]') {
-        myLeads = []
+        myLeads = [];
     } else {
-        render(myLeads)
+        render(myLeads);
     }
 }
 
-inputBtn.addEventListener("click", function() {
-    if(inputEl.value!==""){
-        myLeads[myLeads.length]=inputEl.value
-        render(myLeads)
-        inputEl.value=""
+inputBtn.addEventListener("click", function () {
+    if (inputEl.value !== "") {
+        myLeads[myLeads.length] = inputEl.value;
+        render(myLeads);
+        inputEl.value = "";
     }
-})
+});
 
 //on pressing enter save entry
-inputEl.addEventListener("keyup", function(e) {
-    if(e.key==="Enter"){
+inputEl.addEventListener("keyup", function (e) {
+    if (e.key === "Enter") {
         e.preventDefault();
         inputBtn.click();
     }
-})
+});
 
-tabBtn.addEventListener("click", function() {
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        myLeads[myLeads.length]=tabs[0].url.substring(8)
-        render(myLeads)
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        myLeads[myLeads.length] = tabs[0].url.substring(8);
+        render(myLeads);
     });
-})
+});
 
-deleteBtn.addEventListener("dblclick", function() {
-    localStorage.removeItem("myLeads")
-    myLeads = []
-    render(myLeads)
-})
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.removeItem("myLeads");
+    myLeads = [];
+    render(myLeads);
+});
 
-document.querySelector('body').addEventListener('mousedown', function(event) {
+document.querySelector('body').addEventListener('mousedown', function (event) {
     if (event.target.className.toLowerCase() === 'close-btn') {
-        let closeBtns = document.getElementsByClassName("close-btn")
-        Array.from(closeBtns).forEach(function(closeBtn) {
-            closeBtn.addEventListener("click", function() {
-                myLeads.splice(closeBtn.id.substring(6),1)
-                render(myLeads)
-            })
-        })
+        let closeBtns = document.getElementsByClassName("close-btn");
+        Array.from(closeBtns).forEach(function (closeBtn) {
+            closeBtn.addEventListener("click", function () {
+                myLeads.splice(closeBtn.id.substring(6), 1);
+                render(myLeads);
+            });
+        });
     }
-  });
+});
 
 function render(leads) {
-    ulEl.textContent=""
-    for(let i=0;i<leads.length;i++){
-        const li = document.createElement("li")
-        const a = document.createElement("a")
-        const span = document.createElement("span")
-        let aText = document.createTextNode(leads[i])
-        const spanText = document.createTextNode("X")
-        let aTextStr = aText.wholeText
-        if (aTextStr.length>53) {
-            aText=document.createTextNode(aText.wholeText.substring(0,50)+"...")
+    ulEl.textContent = "";
+    for (let i = 0; i < leads.length; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        const span = document.createElement("span");
+        let aText = document.createTextNode(leads[i]);
+        const spanText = document.createTextNode("X");
+        let aTextStr = aText.wholeText;
+        if (aTextStr.length > 53) {
+            aText = document.createTextNode(aText.wholeText.substring(0, 50) + "...");
         }
-        if (aTextStr.substring(0,4) !== "http") {aTextStr = "https://"+aTextStr}
-        a.setAttribute('href',aTextStr)
-        a.setAttribute('target', '_blank')
-        a.appendChild(aText)
-        span.setAttribute('class','close-btn')
-        span.setAttribute('id','close-'+i)
-        span.appendChild(spanText)
-        li.append(a,span)
-        ulEl.appendChild(li)
+        if (aTextStr.substring(0, 4) !== "http") { aTextStr = "https://" + aTextStr; }
+        a.setAttribute('href', aTextStr);
+        a.setAttribute('target', '_blank');
+        a.appendChild(aText);
+        span.setAttribute('class', 'close-btn');
+        span.setAttribute('id', 'close-' + i);
+        span.appendChild(spanText);
+        li.append(a, span);
+        ulEl.appendChild(li);
     }
-    localStorage.setItem("myLeads",JSON.stringify(leads))
+    localStorage.setItem("myLeads", JSON.stringify(leads));
 }
--- passengercounter/index.js
+++ passengercounter/index.js
@@ -1,21 +1,21 @@
-let countEl = document.getElementById("count-el")
-let saveEl = document.getElementById("save-el")
-document.body.style.backgroundSize="auto "+80*window.devicePixelRatio+"vw";
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+document.body.style.backgroundSize = "auto " + 80 * window.devicePixelRatio + "vw";
 
-let count = 0
+let count = 0;
 
 function increment() {
-    count += 1
-    countEl.innerText = count
+    count += 1;
+    countEl.innerText = count;
 }
 
 function save() {
-    if (count == 0) {}
+    if (count == 0) { }
     else if (saveEl.innerText == "") {
-        saveEl.innerText += "Previous Entries: " + count
+        saveEl.innerText += "Previous Entries: " + count;
     } else {
-    saveEl.innerText += " - " + count
+        saveEl.innerText += " - " + count;
     }
-    count = 0
-    countEl.innerText = count
+    count = 0;
+    countEl.innerText = count;
 }
